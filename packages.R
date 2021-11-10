caa6efced0b9153f7bb59529b2cf5a13edb1ad20
Lynn Bradshaw <readyready15728@gmail.com>
2021-11-09 19:42:44 -0500

    Adding packages.R

---
+++ packages.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+packages <- as_tibble(installed.packages()[,c('Package', 'Version')])
+colnames(packages) <- c('package', 'version') # I just prefer lowercase
+write_csv(packages, 'packages.csv')
