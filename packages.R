47de81b33c40b6c7df8a1fd2a0f4f57634e76145
readyready15728 <readyready15728@gmail.com>
2021-11-21 00:29:54 -0500

    Adding space

--- packages.R
+++ packages.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 
-packages <- as_tibble(installed.packages()[,c('Package', 'Version')])
+packages <- as_tibble(installed.packages()[, c('Package', 'Version')])
 colnames(packages) <- c('package', 'version') # I just prefer lowercase
 write_csv(packages, 'packages.csv')
